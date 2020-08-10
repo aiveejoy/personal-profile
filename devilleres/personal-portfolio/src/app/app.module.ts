69c57824c69a950cc5aedd1342e72874c5375d6d
capstone student <capstonestudent@pn.local>
2020-08-10 09:30:27 +0800

    major changes

--- devilleres/personal-portfolio/src/app/app.module.ts
+++ devilleres/personal-portfolio/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
@@ -20,7 +20,8 @@ import { HobbiesComponent } from './hobbies/hobbies.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
